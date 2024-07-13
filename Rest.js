1f6df3f0d9c9e9d51281c4c3fd6867512b5f1f4b
Salvador <Salvadoralfaro44@gmail.com>
2024-07-13 00:42:05 -0500

    backend

--- Rest.js
+++ Rest.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -5,6 +6,27 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "index.html"));
+//});
+
+const sayHi = (req, res) => {
+  res.send("hi");
+};
+
+app.get("/test/", sayHi);
+
+app.get("/", (req, res) => {
+  res.send("home page")
+})
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  res.send({
+    result: parseInt(a) + parseInt(b)
+  });
+});
+
 app.listen(5000, () => {
-  console.log(`Server is running on port 5000.`);
+  console.log(`Server is running on port http://localhost:5000/`);
 });
